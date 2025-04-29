b1c89a94cf36536f8bed4d2d3e9863ca712f5e57
RickT <2274289773@qq.com>
2025-04-29 13:34:20 +0800

    update

---
+++ 2/env.py
@@ -0,0 +1,6 @@
+from tqdm import tqdm
+from pathlib import Path
+DIR_DATA = Path('data')
+PATH_CORPUS_RAW = DIR_DATA / "ChineseCorpus199801.txt"
+PATH_DICT = DIR_DATA / "dictionary.bin"
+PATH_CORPUS = DIR_DATA / "corpus.bin"
